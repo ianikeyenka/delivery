3c58c957a3141517debf4445c790e6d8d70d72c6
ilya <20prorok02@gmail.com>
2023-12-08 13:22:13 +0300

    add annotation Repository

--- src/main/java/com/example/delivery/repository/ProductRepository.java
+++ src/main/java/com/example/delivery/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.delivery.repository;
 
 import com.example.delivery.model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
 }

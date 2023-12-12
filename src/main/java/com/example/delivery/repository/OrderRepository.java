7b6f874a11c86fe7c1fe8039828af36efa1a1aea
ilya <20prorok02@gmail.com>
2023-12-12 22:22:05 +0300

    fix OrderRepository

--- src/main/java/com/example/delivery/repository/OrderRepository.java
+++ src/main/java/com/example/delivery/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.example.delivery.repository;
 
+import com.example.delivery.model.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<OrderRepository, Long> {
+public interface OrderRepository extends JpaRepository<OrderModel, Long> {
 }

5c1ec187d3d1409b0ac51d875b4f0321db38b30c
ilya <20prorok02@gmail.com>
2023-12-13 17:11:47 +0300

    delete Delivery classes

--- src/main/java/com/example/delivery/repository/DeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.delivery.repository;
-
-import com.example.delivery.model.DeliveryModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryRepository extends JpaRepository<DeliveryModel, Long> {
-}

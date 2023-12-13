34ab2d77b4e10f421ccf1a55256aa8e3d2081d6c
ilya <20prorok02@gmail.com>
2023-12-13 17:52:21 +0300

    add CustomerController

--- src/main/java/com/example/delivery/model/OrderModel.java
+++ src/main/java/com/example/delivery/model/OrderModel.java
@@ -1,17 +1,19 @@
 package com.example.delivery.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -24,8 +26,9 @@ public class OrderModel {
     @Column
     private Long id;
 
-    @OneToMany(mappedBy = "order")
-    private List<CustomerModel> customer;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
 
     @Column
     private String date;
